73168d833b658224654ee29fbf10610b52907535
Iman van Assum <imanvanassum@live.nl>
2016-08-09 01:04:07 +0200

    inserted comment about lack of toLowerCase

--- week4_site/script.js
+++ week4_site/script.js
@@ -52,6 +52,7 @@ WARNING!!! WARNING!!!
     // to lower case character 'j' afterwards.
     // Look up these methods on Mozilla Developer Network web site if needed.
     var firstLetter = names[i].charAt(0);
+    //Meh, screw that toLowerCase method, just did an OR check on capital J as well, easy enough.
 
     // STEP 12:
     // Compare the 'firstLetter' retrieved in STEP 11 to lower case
